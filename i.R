b8263bc4b3251e0669e1aa1364469bffc2f2115b
Ahmadreza Nia <91228905+AhmadrezaNia@users.noreply.github.com>
2024-09-01 18:05:50 -0500

    Update and rename i.R to Lib.R

--- i.R
+++
@@ -1,5 +0,0 @@
-#install.packages("glmnet", repos = "https://cran.us.r-project.org")
-library(glmnet)
-
-#help(matrix)  # To see the documentation of R function
-library(MASS) # for the matrix() function
